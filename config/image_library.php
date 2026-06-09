8066dc83642e55d23cbe8b875e9eb7e9460087df
KBM90 <karim.bouj90@gmail.com>
2026-06-09 19:53:32 +0100

    use unsplash

--- config/image_library.php
+++ config/image_library.php
@@ -1,20 +1,64 @@
 <?php
 
-return [
-    ['key' => 'library/food-1.jpg', 'label' => 'Breakfast Plate'],
-    ['key' => 'library/food-2.jpg', 'label' => 'Croissant'],
-    ['key' => 'library/food-3.jpg', 'label' => 'Burger'],
-    ['key' => 'library/food-4.jpg', 'label' => 'Fresh Salad'],
-    ['key' => 'library/food-5.jpg', 'label' => 'Dessert'],
-    ['key' => 'library/food-6.jpg', 'label' => 'Sandwich'],
-    ['key' => 'library/espresso.jpg', 'label' => 'Espresso'],
-    ['key' => 'library/cappuccino.jpg', 'label' => 'Cappuccino'],
-    ['key' => 'library/iced-coffee.jpg', 'label' => 'Iced Coffee'],
-    ['key' => 'library/soda.jpg', 'label' => 'Lemonade'],
-    ['key' => 'library/smoothie.jpg', 'label' => 'Smoothie'],
-    ['key' => 'library/tea-maroc.jpg', 'label' => 'Tea'],
-    ['key' => 'library/ice-tea.jpg', 'label' => 'Ice Tea'],
-    ['key' => 'library/pizza-1.jpg', 'label' => 'Pizza Fruit de mer'],
-    ['key' => 'library/pizza-2.jpg', 'label' => 'Pizza Fromage'],
-
+$images = [
+    [
+        'key' => 'https://images.unsplash.com/photo-1533089860892-a7c6f0a88666',
+        'label' => 'Breakfast Plate'
+    ],
+    [
+        'key' => 'https://images.unsplash.com/photo-1555507036-ab794f4afe5a',
+        'label' => 'Croissant'
+    ],
+    [
+        'key' => 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd',
+        'label' => 'Burger'
+    ],
+    [
+        'key' => 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd',
+        'label' => 'Fresh Salad'
+    ],
+    [
+        'key' => 'https://images.unsplash.com/photo-1563729784474-d77dbb933a9e',
+        'label' => 'Dessert'
+    ],
+    [
+        'key' => 'https://images.unsplash.com/photo-1528735602780-2552fd46c7af',
+        'label' => 'Sandwich'
+    ],
+    [
+        'key' => 'https://images.unsplash.com/photo-1510707577719-ae7c14805e3a',
+        'label' => 'Espresso'
+    ],
+    [
+        'key' => 'https://images.unsplash.com/photo-1534778101976-62847782c213',
+        'label' => 'Cappuccino'
+    ],
+    [
+        'key' => 'https://images.unsplash.com/photo-1461023058943-07fcbe16d735',
+        'label' => 'Iced Coffee'
+    ],
+    [
+        'key' => 'https://images.unsplash.com/photo-1513558161293-cdaf765ed2fd',
+        'label' => 'Lemonade'
+    ],
+    [
+        'key' => 'https://images.unsplash.com/photo-1502741338009-cac2772e18bc',
+        'label' => 'Smoothie'
+    ],
+    [
+        'key' => 'https://images.unsplash.com/photo-1517701604599-bb29b565090c',
+        'label' => 'Tea'
+    ],
+    [
+        'key' => 'https://images.unsplash.com/photo-1499638673689-79a0b5115d87',
+        'label' => 'Ice Tea'
+    ],
+    [
+        'key' => 'https://images.unsplash.com/photo-1513104890138-7c749659a591',
+        'label' => 'Pizza Fruit de mer'
+    ],
+    [
+        'key' => 'https://images.unsplash.com/photo-1513104890138-7c749659a591',
+        'label' => 'Pizza Fromage'
+    ],
 ];
